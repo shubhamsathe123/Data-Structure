ba973ba293a616d6eb080bb60eeccf7ff8986ddd
shubhamsathe123 <shubhamsathe66@gmail.com>
2021-02-16 10:05:11 +0530

    implement stack using array

---
+++ Ds/src/Stack/StackMethods.java
@@ -0,0 +1,30 @@
+package Stack;
+
+class Stack
+{
+	int arr[]=new int[5];
+	int top=0;
+  public void push(int data)
+  {
+	  arr[top]=data;
+	  top++;
+  }
+  public void display()
+  {
+	  for(int i=0;i<arr.length;i++)
+	  {
+		  System.out.println(arr[i]);
+	  }
+  }
+}
+public class StackMethods {
+ public static void main(String[] args) {
+	Stack s=new Stack();
+	s.push(1);
+	s.push(2);
+	s.push(3);
+	s.push(4);
+	s.push(5);
+	s.display();
+ }
+}
