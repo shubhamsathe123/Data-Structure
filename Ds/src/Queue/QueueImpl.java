b87424c9fa1eff7600f075671b34c3eee67f4b70
shubhamsathe123 <shubhamsathe66@gmail.com>
2021-02-16 12:32:08 +0530

    added enqueue operation

--- Ds/src/Queue/QueueImpl.java
+++ Ds/src/Queue/QueueImpl.java
@@ -1,7 +1,31 @@
 package Queue;
-
+class Queues{
+	int que[]=new int[4];
+	int size=0;
+	int front;
+	int rear=0;
+	public void enqueue(int data)
+	{
+		que[rear]=data;
+		rear++;
+		size++;
+	}
+	public void display()
+	{
+		System.out.println("queue is");
+		for(int i=0;i<size;i++)
+		{
+			System.out.print(que[i]+" ");
+		}
+	}
+}
 public class QueueImpl {
  public static void main(String[] args) {
 	System.out.println("Queue Implementation");
+	Queues q=new Queues();
+	q.enqueue(2);
+	q.enqueue(3);
+	q.display();
+	
 }
 }
